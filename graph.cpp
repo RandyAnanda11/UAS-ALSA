a646e1b55b95e9b87ce537349a1875b3612afcfb
Randy Ananda <88238381+RandyAnanda11@users.noreply.github.com>
2023-07-16 10:36:45 +0700

    Update graph.cpp
    FINAL

--- graph.cpp
+++ graph.cpp
@@ -14,33 +14,32 @@ struct simpul {
 
 simpul* p, * first, * last, * Q, * R;
 simpul* points[4];
-
-int main() {
-    int A[4][4] = {
+int A[4][4] = {
         {0, 6, 9, 5},
         {6, 0, 2, 8},
         {5, 2, 0, 7},
         {9, 8, 7, 0},
     };
 
-    string AbjadSimpul[] = {"K", "L", "M", "N"};
+    string namaKota[] = {"K", "L", "M", "N"};
     int i, j;
     int urutanTerpilih[4];
+    int banyakKota = sizeof(namaKota) / sizeof(namaKota[0]);
 
+void TampilkanJarakAntarKota () {
     i = 0;
     j = 0;
     p = new simpul;
-
-    p->info = AbjadSimpul[0];
+    p->info = namaKota[0];
     first = p;
     last = p;
     p->left = NULL;
     p->right = NULL;
     points[0] = p;
 
-    for (i = 1; i < sizeof(AbjadSimpul) / sizeof(AbjadSimpul[0]); i++) {
+    for (i = 1; i < banyakKota; i++) {
         p = new simpul;
-        p->info = AbjadSimpul[i];
+        p->info = namaKota[i];
         last->left = p;
         last = last->left;
         p->left = NULL;
@@ -49,17 +48,18 @@ int main() {
     }
 
     Q = first;
-    for (i = 0; i < sizeof(AbjadSimpul) / sizeof(AbjadSimpul[0]); i++) {
+    for (i = 0; i < banyakKota; i++) {
         R = Q;
-        cout << "vertex " << Q->info << endl;
-        for (j = 0; j < sizeof(AbjadSimpul) / sizeof(AbjadSimpul[0]); j++) {
+        cout << "-------------------------------------" << endl;
+        cout << "Jarak Kota " << Q->info << endl;
+        for (j = 0; j < banyakKota; j++) {
             if (A[i][j] != 0) {
                 p = new simpul;
-                p->info = AbjadSimpul[j];
+                p->info = namaKota[j];
                 R->right = p;
                 p->left = points[j];
-                cout << "berhubungan dengan: " << p->left->info << endl;
-                cout << "bobot: " << A[i][j] << endl;
+                cout << "Ke Kota " << p->left->info << endl;
+                cout << "Adalah: " << A[i][j] << " km" << endl;
                 p->right = NULL;
                 R = p;
             }
@@ -68,25 +68,29 @@ int main() {
         Q = Q->left;
     }
 
+}
+
+void HitungJarakTerpendek () {
     // Mencari jalur terpendek untuk memutari seluruh kota
     string vertexMulai;
-    cout << "Masukkan vertex mulai: ";
+    cout << "---------------------------------------------------------"<<endl;
+    cout << "Masukkan Kota dimana anda akan memulai ekspedisi anda: ";
     cin >> vertexMulai;
 
     int urutanKunjungan[4];
     int indexAwal;
 
     // Mencari indeks dari vertex mulai
-    for (i = 0; i < sizeof(AbjadSimpul) / sizeof(AbjadSimpul[0]); i++) {
-        if (AbjadSimpul[i] == vertexMulai) {
+    for (i = 0; i < banyakKota; i++) {
+        if (namaKota[i] == vertexMulai) {
             indexAwal = i;
             break;
         }
     }
 
     // Menginisialisasi urutan kunjungan
-    for (i = 0; i < sizeof(AbjadSimpul) / sizeof(AbjadSimpul[0]); i++) {
-        urutanKunjungan[i] = (indexAwal + i) % (sizeof(AbjadSimpul) / sizeof(AbjadSimpul[0]));
+    for (i = 0; i < banyakKota; i++) {
+        urutanKunjungan[i] = (indexAwal + i) % (banyakKota);
     }
 
     int jarakTerpendek = INF; // Men-set jarak terpendek dengan nilai yang sangat besar
@@ -95,7 +99,7 @@ int main() {
     do {
         int Jarak = 0;
 
-        for (i = 0; i < sizeof(AbjadSimpul) / sizeof(AbjadSimpul[0]) - 1; i++) {
+        for (i = 0; i < banyakKota - 1; i++) {
             int from = urutanKunjungan[i];
             int to = urutanKunjungan[i + 1];
             Jarak += A[from][to];
@@ -103,20 +107,52 @@ int main() {
 
         // Memperbarui jarak terpendek jika ditemukan jarak yang lebih kecil
         if (Jarak < jarakTerpendek) {
-            for (int i = 0; i < sizeof(AbjadSimpul) / sizeof(AbjadSimpul[0]); i++) {
+            for (int i = 0; i < banyakKota; i++) {
                 urutanTerpilih[i] = urutanKunjungan[i];
             }
             jarakTerpendek = Jarak;
         }
-    } while (next_permutation(urutanKunjungan + 1, urutanKunjungan + sizeof(AbjadSimpul) / sizeof(AbjadSimpul[0])));
+    } while (next_permutation(urutanKunjungan + 1, urutanKunjungan + banyakKota));
 
     // Menampilkan jarak terpendek yang dapat dicapai
-    cout << "Jarak terpendek yang dapat dicapai: " << jarakTerpendek << endl;
+    cout << "Jarak terpendek yang dapat dicapai: " << jarakTerpendek << " Km" << endl;
     cout << "Dengan urutan kunjungan sebagai berikut: ";
-    for (int i = 0; i < sizeof(AbjadSimpul) / sizeof(AbjadSimpul[0]); i++) {
-        cout << AbjadSimpul[urutanTerpilih[i]] << " ";
+    for (int i = 0; i < banyakKota; i++) {
+        cout << namaKota[urutanTerpilih[i]] << " ";
     }
     cout << endl;
+    cout <<"---------------------------------------------"<<endl;
+}
 
+void TampilkanMenu () {
+    cout << endl;
+    cout << "-----------------------------------------------------------"<<endl;
+    cout << "1. Tampilkan jarak antar kota" << endl;
+    cout << "2. Cari jarak terpendek" << endl;
+    cout << "3. EXIT"<<endl;
+    cout << "------------------------------------------------------------" << endl;
+
+
+}
+int main() {
+
+    cout << "Selamat datang, Sales Randy. Untuk memulai ekspedisi Anda, silakan pilih menu (masukkan angka):" << endl;
+    int pilihan;
+    do{
+        TampilkanMenu();
+        cin >> pilihan;
+        switch (pilihan) {
+            case 1:
+                TampilkanJarakAntarKota();
+                break;
+            case 2:
+                HitungJarakTerpendek();
+                break;
+            case 3:
+                cout << "Exiting The Program..." << endl;
+                break;
+            }
+    } while (pilihan != 3);
+    
     return 0;
 }
